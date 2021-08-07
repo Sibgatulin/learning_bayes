006d38bc2b027c9fdd568c856454281d4d004e06
Sibgatulin <renat.sibgatulin@gmail.com>
2021-08-07 14:07:49 +0200

    Init commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# Snippets on probabilistic modelling without clear belonging
---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import setup, find_packages
+
+
+def readme():
+    with open("README.md") as f:
+        f.read()
+
+
+setup(
+    name="learing_bayes",
+    version="0.1",
+    description="Collection of snippets on questions in Bayesian inference",
+    long_description=readme(),
+    url="https://github.com/Sibgatulin/learing_bayes/",
+    author="Renat Sibgatulin",
+    packages=find_packages(),
+    install_requires=[
+        "jax",
+        "numpy",
+        "numpyro",
+        "funsor",
+        "jupyter",
+        "matplotlib",
+    ],
+)
